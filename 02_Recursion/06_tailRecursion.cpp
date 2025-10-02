5fc0cac0465fd11e8bbf47b92c138964bfd36f81
sakshijadhav0703 <sakshijadhav8856@gmail.com>
2025-10-02 12:00:17 +0530

    Tail Recursion program

---
+++ 02_Recursion/06_tailRecursion.cpp
@@ -0,0 +1,15 @@
+// Tail recursion means last call of function is recursive call.
+
+#include<iostream>
+using namespace std;
+
+int fun(int n) {
+    if(n>0) {
+        cout<<n;
+        fun(n-1);
+    }
+}
+int main() {
+    fun(3);
+    return 0;
+}
