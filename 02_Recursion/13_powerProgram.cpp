c89f3be0e34130c7468779e435550a752baf1968
sakshii-sj07 <sakshijadhav8856@gmail.com>
2025-10-04 01:03:31 +0530

    Power of any number program

---
+++ 02_Recursion/13_powerProgram.cpp
@@ -0,0 +1,26 @@
+// power of any number 
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int power(int m , int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return pow(m , n-1) * m;
+    }
+}
+
+int main() {
+    int num;
+    cout<<"Enter number : ";
+    cin>>num;
+
+    int pow;
+    cout<<"Enter power : ";
+    cin>>pow;
+
+    cout<<power(num , pow);
+    return 0;
+}
