e50a7b56123d1e43037c42891b1649e122abdb1b
sakshii-sj07 <sakshijadhav8856@gmail.com>
2025-10-06 10:13:56 +0530

    Static array program

---
+++ 03_Array/02_staticArray.cpp
@@ -0,0 +1,14 @@
+// static array
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int A[5] = {2,4,6,8,10};
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    
+    return 0;
+}
