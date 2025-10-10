9f544da698926ec69a9afcbc9129c91c0430bb20
sakshii-sj07 <sakshijadhav8856@gmail.com>
2025-10-10 23:05:55 +0530

    Maximum element of array

---
+++ 03_Array/19_maxElement.cpp
@@ -0,0 +1,52 @@
+// Finding maximum element in array
+
+#include<iostream>
+using namespace std;
+
+struct Array {
+    int *A;
+    int size;
+    int length;
+};
+
+void Display(struct Array arr) {
+    cout<<"Elements are : ";
+    for (int i = 0; i < arr.length; i++)
+    {
+        cout<<arr.A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int maxElement(struct Array arr) {
+    int max = arr.A[0];
+    for (int i = 0; i < arr.length; i++)
+    {
+        if(arr.A[i] > max) {
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+int main() {
+    struct Array arr;
+    cout<<"Enter total space in array : ";
+    cin>>arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    int n;
+    cout<<"Enter elements in array : ";
+    cin>>n;
+
+    cout<<"Enter "<<n<<" elements : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr.A[i];
+        arr.length = n;
+    }
+    
+    Display(arr);
+    cout<<"Maximum element is "<<maxElement(arr);
+    return 0;
+}
