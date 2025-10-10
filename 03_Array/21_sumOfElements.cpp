8fdc35fbf414860736e34855610c7cb10917e9e6
sakshii-sj07 <sakshijadhav8856@gmail.com>
2025-10-10 23:56:48 +0530

    Sum of elements in array

---
+++ 03_Array/21_sumOfElements.cpp
@@ -0,0 +1,51 @@
+// sum of elements in an Array
+
+#include<iostream>
+using namespace std;
+
+struct Array {
+    int *A;
+    int size;
+    int length;
+};
+
+void Display(struct Array arr) {
+    cout<<"Elements are : ";
+    for (int i = 0; i < arr.length; i++)
+    {
+        cout<<arr.A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int sum(struct Array arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.length; i++)
+    {
+        sum = sum + arr.A[i];
+    }
+    return sum;
+}
+int main() {
+    struct Array arr;
+    cout<<"Enter totalspace in array : ";
+    cin>>arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    int n;
+    cout<<"Enter elements inn array : ";
+    cin>>n;
+
+    cout<<"Enter "<<n<<" Elements : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr.A[i];
+        arr.length = n;
+    }
+    
+    Display(arr);
+
+    cout<<"Sum is "<<sum(arr);
+    return 0;
+}
