4be1c5c79b2565063fb1e5fb87bf4a2f1480b0c4
sakshii-sj07 <sakshijadhav8856@gmail.com>
2025-10-13 19:15:18 +0530

    Missing elements

---
+++ 03_Array/41_missingElements4.cpp
@@ -0,0 +1,72 @@
+// missing elements in unordred Array. this is more efficient because it takess less time.
+
+#include<iostream>
+using namespace std;
+
+class Array {
+    public:
+    int *A;
+    int *H;
+    int size;
+    int length;
+};
+
+void Display(Array arr) {
+    cout<<"Elements are : ";
+    for (int i = 0; i < arr.length; i++)
+    {
+        cout<<arr.A[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int missingElement(Array &arr) {
+    int l = arr.A[0];
+    int h = arr.A[0];
+
+    for (int i = 0; i < arr.length; i++)
+    {
+        if(arr.A[i] > h ) {
+            h = arr.A[i];
+        }
+    }
+    
+    
+    arr.H = new int[h + 1] {0};
+
+    for (int i = 0; i < arr.length; i++)
+    {
+        arr.H[arr.A[i]]++;
+    }
+    for (int i = l; i <= h; i++)
+    {
+        if(arr.H[i] == 0) {
+            cout<<i<<" ";
+        }
+    }
+    
+    
+}
+int main() {
+    Array arr;
+    cout<<"Enter total space in Array : ";
+    cin>>arr.size;
+
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    int n;
+    cout<<"Enter elements in array : ";
+    cin>>n;
+
+    cout<<"Enter "<<n<<" elements : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr.A[i];
+        arr.length = n;
+    }
+    
+    cout<<"Missing elements : ";
+    missingElement(arr);
+    return 0;
+}
